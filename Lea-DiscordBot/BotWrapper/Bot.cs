1e0bbb3ac556b637686575703d97a41f95be9b2d
Dramiel Leayal <whitered12316@gmail.com>
2017-04-22 10:28:47 +0700

    Initial Commit

---
+++ Lea-DiscordBot/BotWrapper/Bot.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Leayal;
+
+namespace LeaDiscordBot.BotWrapper
+{
+    public class Bot : System.IDisposable
+    {
+        public const string BotVersion = "0.0.1";
+        private DiscordSocketClient client;
+        private char commandPrefix;
+        private ulong ownerID;
+
+        public Bot(char cmdPrefix)
+        {
+            System.Console.WriteLine("Creating Bot instance");
+            //this.ownerID = 1;
+            this.commandPrefix = cmdPrefix;
+            this.client = new DiscordSocketClient(new DiscordSocketConfig()
+            {
+                MessageCacheSize = 1,
+                LogLevel = Discord.LogSeverity.Error,
+                HandlerTimeout = 5000,
+                ConnectionTimeout = 3000,
+                DefaultRetryMode = Discord.RetryMode.Retry502 | Discord.RetryMode.RetryTimeouts
+            });
+            this.client.LoggedIn += Client_LoggedIn;
+            this.client.LoggedOut += Client_LoggedOut;
+            this.client.MessageReceived += Client_MessageReceived;
+            this.client.Log += Client_Log;
+            //this.commander = new CommandService(new CommandServiceConfig() { CaseSensitiveCommands = false });
+        }
+
+        private async Task Client_LoggedIn()
+        {
+            System.Console.WriteLine("Bot logged in. Ready~!");
+            var myInfo = await client.GetApplicationInfoAsync();
+            System.Console.WriteLine(string.Format("Owner: {0}{1}", myInfo.Owner.Username, string.IsNullOrEmpty(myInfo.Owner.Discriminator) ? string.Empty : "#" + myInfo.Owner.Discriminator));
+            this.ownerID = myInfo.Owner.Id;
+            await Task.Yield();
+        }
+
+        public async Task Login(string botToken)
+        {
+            System.Console.WriteLine("Logging in");
+            await this.client.LoginAsync(TokenType.Bot, botToken);
+            await this.client.StartAsync();
+        }
+
+        private async Task Client_MessageReceived(SocketMessage message)
+        {
+            if (!string.IsNullOrWhiteSpace(message.Content))
+                if (message.Author != this.client.CurrentUser)
+                {
+                    foreach (var mentioned in message.MentionedUsers)
+                        if (mentioned.Id == this.client.CurrentUser.Id)
+                        {
+                            await message.Channel.SendMessageAsync("Someone pinged me????");
+                            break;
+                        }
+                    // Command type with command prefix ????
+                    if (message.Content.IndexOf(this.commandPrefix) == 0)
+                    {
+                        string lowercontext = message.Content.ToLower();
+                        // Remove the command prefix and start to parse the command
+                        string[] splittedMsg = lowercontext.Remove(0, 1).Split(new char[] { ' ' }, System.StringSplitOptions.RemoveEmptyEntries);
+                        // Someone ordered me to do something ???
+                        switch(splittedMsg[0])
+                        {
+                            case "info":
+                                // Introduce myself
+                                EmbedBuilder ebuilder = new EmbedBuilder();
+                                //ebuilder.Url = "";
+                                ebuilder.Description = "Why would you want to know of me??? Just take it simple: I'm a bot.";
+                                ebuilder.ThumbnailUrl = this.client.CurrentUser.GetAvatarUrl();
+                                ebuilder.AddField("Version", BotVersion);
+                                ebuilder.Title = "Lea-DiscordBot";
+                                var myauthor = new EmbedAuthorBuilder();
+                                var authorObject = this.client.GetUser(164090291421184002);
+                                myauthor.Name = authorObject.Username;
+                                myauthor.IconUrl = authorObject.GetAvatarUrl();
+                                ebuilder.Author = myauthor;
+                                //ebuilder.
+                                await message.Channel.SendMessageAsync("", false, ebuilder.Build());
+                                break;
+                            case "shutdown":
+                                if (message.Author.Id == this.ownerID)
+                                {
+#pragma warning disable 4014
+                                    this.client.LogoutAsync();
+#pragma warning restore 4014
+                                    await this.client.StopAsync();                                    
+                                    System.Environment.Exit(0);
+                                }
+                                return;
+                            case "error":
+                                if (splittedMsg.Length > 1)
+                                    for (int i = 1; i < splittedMsg.Length; i++)
+                                        if (NumberHelper.TryParse(splittedMsg[i], out var theCode))
+                                        {
+                                            EmbedBuilder errorExplainationbuilder = new EmbedBuilder()
+                                            {
+                                                Description = GetErrorExplaination(theCode),
+                                                Title = string.Format("Error Code: {0}", theCode)
+                                            };
+                                            await message.Channel.SendMessageAsync("", false, errorExplainationbuilder.Build());
+                                        }
+                                break;
+                            //case "wiki": let the wiki come later
+                                
+                            default:
+                                System.Console.WriteLine(string.Format("'{0}' (ID: {1}) ordered '{2}' but this command is not existed.", message.Author.Username, message.Author.Id, splittedMsg[0]));
+                                break;
+                        }
+                    }
+
+                    // Non-command type
+                    //if (string.Compare(message.Content, "error", true) == 0)
+                    else if (lowercontext.IndexOf("error") == 0)
+                    {
+                        //Split the params out
+                        //string[] splittedMsg = message.Content.Split(new char[] { ' ' }, System.StringSplitOptions.RemoveEmptyEntries);
+                    }
+                }
+        }
+
+        private string GetErrorExplaination(int errorCode)
+        {
+            switch(errorCode)
+            {
+                case 630:
+                    return "This mean your connection to the game server has been terminated due to some certain reasons.";
+                case 816:
+                    return "This mean you have been temporarily banned. Either contact SEGA or waiting for miracle. Wish best luck to you~!";
+                case 817:
+                    return "This mean you have been permanent banned. Dudu once said: \"Your luck is extraordinary terrible\".";
+                default:
+                    return "Unknown error or laiwhgliawgilawhg";
+            }
+        }
+
+        private async Task Client_LoggedOut()
+        {
+            await Task.Yield();
+            System.Environment.Exit(0);
+        }
+
+        private async Task Client_Log(LogMessage arg)
+        {
+            await Task.Yield();
+        }
+
+        public async Task Logout()
+        {
+            await this.client.LogoutAsync();
+        }
+
+        public void Dispose()
+        {
+            this.client.Dispose();
+        }
+    }
+}
---
+++ Lea-DiscordBot/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaDiscordBot
+{
+    class Program
+    {
+        private static BotWrapper.Bot myBot;
+        private static Leayal.Ini.IniFile _configFile;
+        private static Leayal.Ini.IniFile ConfigFile
+        {
+            get
+            {
+                // Use current WORKING directory
+                if (_configFile == null)
+                    _configFile = new Leayal.Ini.IniFile(System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "bot.ini"));
+                return _configFile;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.CancelKeyPress += Console_CancelKeyPress;
+            ShowMainMenu();
+        }
+
+        public static void ShowMainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Hello User~! I am Lea-DiscordBot. I am awaiting for your order:");
+            Console.WriteLine("1. Launch DiscordBot");
+            Console.WriteLine("2. Settings");
+            Console.WriteLine();
+            Console.WriteLine("0. Quit");
+            Console.Write("Press the number without pressing enter: ");
+            ConsoleKeyInfo ki = AwaitingOrder(false, '0', '1', '2');
+            switch (ki.KeyChar)
+            {
+                case '1':
+                    // Launch Bot
+                    Console.Clear();
+                    try
+                    {
+                        string myKey = ConfigFile.GetValue("Bot", "Token", string.Empty);
+                        if (string.IsNullOrWhiteSpace(myKey))
+                            throw new ArgumentNullException("You haven't set the BOT token.");
+                        else
+                        {
+                            string cmdPrefix = ConfigFile.GetValue("Bot", "CommandPrefix", string.Empty);
+                            if (string.IsNullOrWhiteSpace(cmdPrefix))
+                                cmdPrefix = "'";
+                            MainAsync(cmdPrefix[0], myKey).GetAwaiter().GetResult();
+                            BlockExit(false);
+                            Logout();
+                            System.Threading.Thread.Sleep(500);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                        Console.WriteLine("Press any key to close");
+                        AwaitingOrder(false, null);
+                    }
+                    break;
+                case '2':
+                    // Launch Settings
+                    LaunchConfigPanel();
+                    break;
+                default:
+                    Environment.Exit(0);
+                    return;
+            }
+        }
+
+        private static void BlockExit(bool intercept)
+        {
+            ConsoleKeyInfo theKey = Console.ReadKey(intercept);
+            if (theKey.Key != ConsoleKey.Escape)
+                BlockExit(intercept);
+        }
+
+        private static void LaunchConfigPanel()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose setting:");
+            Console.WriteLine("1. Set Bot token");
+            Console.WriteLine("2. Set Bot prefix");
+            Console.WriteLine();
+            Console.WriteLine("0. Back to main menu");
+            Console.Write("Press the number without pressing enter: ");
+            ConsoleKeyInfo ki = AwaitingOrder(false, '0', '1', '2');
+            switch (ki.KeyChar)
+            {
+                case '0':
+                    ShowMainMenu();
+                    break;
+                case '1':
+                    Console.Clear();
+                    Console.WriteLine("Please leave the Bot's token below and then press Enter to confirm... or press Esc to cancel:");
+                    //Tricky ???
+                    StringBuilder sb = new StringBuilder();
+                    if (ConsoleReadline(sb) != null)
+                    {
+                        ConfigFile.SetValue("Bot", "Token", sb.ToString());
+                        ConfigFile.Save();
+                        Console.Clear();
+                        Console.WriteLine("Config saved.");
+                        Console.WriteLine("Press any key to go back");
+                        AwaitingOrder(false, null);
+                        LaunchConfigPanel();
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Token settings cancelled");
+                        Console.WriteLine("Press any key to go back");
+                        AwaitingOrder(false, null);
+                        LaunchConfigPanel();
+                    }
+                    break;
+            }
+        }
+
+        private static StringBuilder ConsoleReadline(StringBuilder sb)
+        {
+            var pressedKey = Console.ReadKey(false);
+            switch(pressedKey.Key)
+            {
+                case ConsoleKey.Enter:
+                    return sb;
+                case ConsoleKey.Escape:
+                    return null;
+                default:
+                    sb.Append(pressedKey.KeyChar);
+                    return ConsoleReadline(sb);
+            }
+
+        }
+
+        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            e.Cancel = true;
+            Logout();
+        }
+
+        private static async Task MainAsync(char cmdPrefix, string token)
+        {
+            myBot = new BotWrapper.Bot(cmdPrefix);
+            await myBot.Login(token);
+        }
+
+        private static void Logout()
+        {
+            myBot.Logout().GetAwaiter().GetResult();
+        }
+
+        private static ConsoleKeyInfo AwaitingOrder(bool intercept, params char[] allowedChars)
+        {
+            ConsoleKeyInfo theKey = Console.ReadKey(intercept);
+            if (allowedChars != null && allowedChars.Length > 0)
+            {
+                for (int i = 0; i < allowedChars.Length; i++)
+                    if (allowedChars[i] == theKey.KeyChar)
+                        return theKey;
+                return AwaitingOrder(intercept, allowedChars);
+            }
+            else
+                return theKey;
+        }
+    }
+}
